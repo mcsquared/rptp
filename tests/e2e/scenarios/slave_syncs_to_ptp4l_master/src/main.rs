1940986fe57f0d039b96a38dfe39efdd51531af6
Daniel Waschina <daniel.waschina@posteo.de>
2025-10-23 22:24:23 +0200

    slave syncs to ptp4l master e2e test

---
+++ tests/e2e/scenarios/slave_syncs_to_ptp4l_master/src/main.rs
@@ -0,0 +1,91 @@
+use std::rc::Rc;
+
+use tokio::net::UdpSocket;
+use tokio::sync::mpsc;
+use tokio::time::{Duration, timeout};
+
+use rptp::bmca::{FullBmca, LocalClockDS};
+use rptp::clock::{ClockIdentity, ClockQuality, FakeClock, LocalClock};
+use rptp::infra::infra_support::SortedForeignClockRecordsVec;
+use rptp::port::{DomainPort, PortNumber, SingleDomainPortMap};
+use rptp::portstate::PortState;
+use rptp::time::TimeStamp;
+use rptp_daemon::net::MulticastSocket;
+use rptp_daemon::node::{TokioNetwork, TokioPhysicalPort, TokioTimerHost};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> std::io::Result<()> {
+    let domain = 0;
+
+    let local_clock = LocalClock::new(
+        Rc::new(FakeClock::new(TimeStamp::new(0, 0))),
+        LocalClockDS::new(
+            ClockIdentity::new(&[0x00, 0x1B, 0x19, 0xFF, 0xFE, 0x00, 0x00, 0x02]),
+            ClockQuality::new(250, 0xFE, 0xFFFF),
+        ),
+    );
+    let event_socket = Rc::new(MulticastSocket::event().await?);
+    let general_socket = Rc::new(MulticastSocket::general().await?);
+
+    let (system_tx, system_rx) = mpsc::unbounded_channel();
+    let physical_port = TokioPhysicalPort::new(
+        &local_clock,
+        domain,
+        event_socket.clone(),
+        general_socket.clone(),
+        system_tx.clone(),
+    );
+    let bmca = FullBmca::new(SortedForeignClockRecordsVec::new());
+    let timer_host = TokioTimerHost::new(domain, system_tx);
+
+    let port_state = PortState::initializing(DomainPort::new(
+        &local_clock,
+        bmca,
+        physical_port,
+        timer_host,
+        domain,
+        PortNumber::new(1),
+    ));
+
+    let portmap = SingleDomainPortMap::new(domain, port_state);
+
+    let tokio_network = TokioNetwork::new(
+        &local_clock,
+        portmap,
+        event_socket,
+        general_socket,
+        system_rx,
+    )
+    .await?;
+
+    println!("Slave ready");
+
+    let clock_sync = async {
+        loop {
+            tokio::time::sleep(Duration::from_millis(100)).await;
+
+            // TODO: this is a quite weak acceptance criteria for clock sync, so it's probably
+            // better to improve it in the future, to determine the ptp4l master's time through
+            // some side channel. However, it indicates at least the successful message exchange
+            // and clock update against ptp4l, which is already a good start.
+            if local_clock.now() != TimeStamp::new(0, 0) {
+                break;
+            }
+        }
+    };
+
+    let result = timeout(Duration::from_secs(30), tokio_network.run_until(clock_sync))
+        .await
+        .map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::TimedOut,
+                "Slave node timed out before clock sync",
+            )
+        })?;
+
+    let socket = UdpSocket::bind("0.0.0.0:12345").await?;
+    socket.set_broadcast(true)?;
+    socket.send_to(b"accept", "255.255.255.255:12345").await?;
+
+    result
+}
---
+++ tests/e2e/tests/slave_syncs_to_ptp4l_master.rs
@@ -0,0 +1,61 @@
+use std::time::Duration;
+
+use bollard::Docker;
+use testcontainers::{GenericImage, ImageExt, core::WaitFor, runners::AsyncRunner};
+
+use rptp_e2e_tests::{PrintLog, TestContainer, TestImage, TestNetwork};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    #[ignore]
+    async fn slave_syncs_to_ptp4l_master() -> anyhow::Result<()> {
+        let docker = Docker::connect_with_local_defaults()?;
+
+        let image = TestImage::new(docker.clone(), "e2e-scenarios");
+        let tag = image.build().await?;
+
+        let net = TestNetwork::new(docker.clone(), "ptpnet").await?;
+
+        let master = TestContainer::new(
+            GenericImage::new(image.name(), &tag)
+                .with_wait_for(WaitFor::message_on_stdout("assuming the grand master role"))
+                .with_log_consumer(PrintLog { prefix: "ptp4l" })
+                .with_cmd(["ptp4l", "-i", "eth0", "-S", "-m", "-l", "6", "-4"])
+                .with_network(net.name())
+                .start()
+                .await?,
+            docker.clone(),
+        );
+
+        let slave = TestContainer::new(
+            GenericImage::new(image.name(), &tag)
+                .with_wait_for(WaitFor::message_on_stdout("Slave ready"))
+                .with_log_consumer(PrintLog { prefix: "slave" })
+                .with_cmd(["/app/slave-syncs-to-ptp4l-master"])
+                .with_network(net.name())
+                .start()
+                .await?,
+            docker.clone(),
+        );
+
+        let result = slave.wait_for_exit(Duration::from_secs(60)).await;
+
+        match result {
+            Ok(slave_status) => {
+                assert!(slave_status == 0);
+            }
+            Err(_) => {
+                panic!("Test failed due to error or timeout");
+            }
+        }
+
+        slave.rm().await?;
+        master.stop().await?;
+        master.rm().await?;
+
+        Ok(())
+    }
+}
